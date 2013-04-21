086b7b5b2744d502e2369b766edcdf68fccf0353
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-22 01:15:03 +0300

    Adding Pelican based site generation

---
+++ docs/content/pages/irc.rst
@@ -0,0 +1,18 @@
+IRC
+===
+
+.. contents :: :local:
+
+IRC, internet relat chat, is a chat network protocol for real-time chatting.
+
+* There exist many separate IRC networks, `Freenode <>`_ being most popular
+
+* People connect to IRC
+
+* If you need to
+
+How to connect to IRC
+----------------------
+
+Persistent connections
+-------------------------
---
+++ docs/pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Mikko Ohtamaa'
+SITENAME = u'Internet chat questions and etique guide'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+        ('Python.org', 'http://python.org'),
+        ('Jinja2', 'http://jinja.pocoo.org'),
+        ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+THEME = "theme"
+
+DISPLAY_PAGES_ON_MENU = True
+
