de167f7bdca547f03ba6807a1b21d171083baaee
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-26 22:11:26 +0300

    Front page shaping up

--- docs/theme/static/main.js
+++ docs/theme/static/main.js
@@ -2,10 +2,24 @@
 
     "use strict";
 
-    function init() {
+    function setupGuidedArrival() {
         if(window.location.hash == "#help") {
             $("#guided-arrival").show();
         }
+    }
+
+    function setupMoreInfo() {
+        $(".advice").on("click", ".more-info-toggle", function(e) {
+            e.preventDefault();
+            var moreInfo = $(e.delegateTarget).find(".more-info");
+            moreInfo.slideToggle();
+            return false;
+        });
+    }
+
+    function init() {
+        setupGuidedArrival();
+        setupMoreInfo();
     }
 
     $(init);
